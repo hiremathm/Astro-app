ea0aa78c72ce76a4c4d804814ca8ca3950557f94
Shivakummara M <shivasorab@gmail.com>
2018-08-18 09:59:21 +0530

    password changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'shivasorab@gmail.com', password: 'password', password_confirmation: 'shivam') if Rails.env.development?
+AdminUser.create!(email: 'shivasorab@gmail.com', password: 'shivam', password_confirmation: 'shivam') if Rails.env.development?
